31761377b724ec6b1e69342018ae1d613c629fb5
huangmingyu <930331503@qq.com>
2023-02-05 09:30:04 +0800

    feat: init vue2 project;

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  /* 1、cannot resolve */
+  background: url("./assets/logo.png");
+}
+
+#app {
+  /* 2、can resolve */
+  background: url("@/assets/logo.png");
+}
+</style>
